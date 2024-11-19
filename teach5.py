1f69af98460ea8258076544865ac46e01a9e799f
David Willis <149202123+david-willis-dev@users.noreply.github.com>
2024-11-18 20:23:58 -0500

    Create teach5.py

---
+++ teach5.py
@@ -0,0 +1,66 @@
+#Hello! For this activity, start with program 1 and see if you can update the program with the suggestions. When you are ready simply move to the next program and remove the comments from the code to run it.
+
+#------------------PROGRAM 1--------------------
+#Can you modify this program to print a different message?
+#Can you modify this program to print two messages?
+
+#Code:
+print("Hello World!")
+
+
+
+
+#------------------PROGRAM 2--------------------
+#Can you modify the program to subtract 4 and 7?
+#Can you modify the program to add 4, 7, and 9?
+
+#Code:
+#print("Adding 4 and 7")
+#num1 = 4
+#num2 = 7
+#result = num1 + num2
+#print(result)
+
+
+
+
+#------------------PROGRAM 3--------------------
+#Modify the program to also do each of these:
+#Subtract the numbers and print the solution
+#Multiply the numbers and print the solution
+#Divide the numbers and print the solution
+
+#Code:
+# print("Enter first number: ")
+# num1 = int(input())
+# print("Enter second number: ")
+# num2 = int(input())
+# print("Adding numbers…")
+# result = num1 + num2
+# print(result)
+
+
+
+
+#------------------PROGRAM 4--------------------
+#Can you modify the program to also allow multiplication and division?
+
+# Code:
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# operator = input("Enter the operation (+, -): ")
+# if operator == "+":
+#     print(num1 + num2)
+# elif operator == "-":
+#     print(num1 - num2)
+
+
+
+  
+#------------------PROGRAM 5--------------------
+#Use what you've learned so far to program your own calculator. Feel free to re-use code. Can you include these things:
+#Allow user to do multiple subsequent operations
+#Allow exponent operations
+#Allow user to perform operations on more than 2 numbers
+
+#Code:
